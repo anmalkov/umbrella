cdeaca8290ada87be126e07263e17966c37933e0
anmalkov <an.malkov@outlook.com>
2022-10-11 12:05:18 +0200

    show widgets on dashboard

--- src/Umbrella.Ui/ClientApp/src/components/Dashboard.js
+++ src/Umbrella.Ui/ClientApp/src/components/Dashboard.js
@@ -1,10 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Row } from 'reactstrap';
+import Widget from './Widget';
 
 const Dashboard = () => {
+
+    const widgets = [
+        { id: 'w1', col: 1, position: 1, enities: ['hue.light.test1'] },
+        { id: 'w2', col: 2, position: 2, enities: ['hue.light.test2'] },
+        { id: 'w3', col: 2, position: 1, enities: ['hue.light.test3'] },
+        { id: 'w4', col: 4, position: 1, enities: ['hue.light.test4'] }
+    ];
+
+    const [widgetsList, setWidgetsList] = useState(widgets);
+
+    if (widgetsList.length === 0) {
+        return (
+            <div>
+                <p>There are no widgets</p>
+            </div>
+        );
+    }
+
     return (
-      <div>
-        <h1>Dashboard</h1>
-      </div>
+        <Row>
+            { [1, 2, 3, 4].map(col => (
+                <div class="col-lg-3 col-md-6 col-sm-12">
+                    {widgetsList.filter(w => w.col === col).sort((a, b) => a.position > b.position ? 1 : -1).map(w => (
+                        <Widget />
+                     ))}
+                </div>
+            ))}
+        </Row>
     );
 }
 
---
+++ src/Umbrella.Ui/ClientApp/src/components/Widget.js
@@ -0,0 +1,15 @@
+﻿import React from 'react';
+import { Card, CardBody, CardTitle, CardText } from 'reactstrap';
+
+const Widget = () => {
+    return (
+        <Card className="widget">
+            <CardBody>
+                <CardTitle>Widget</CardTitle>
+                <CardText>Widget content</CardText>
+            </CardBody>
+        </Card>
+    );
+}
+
+export default Widget;
