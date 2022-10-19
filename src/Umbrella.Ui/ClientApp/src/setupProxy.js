2e18399ca3e02b3d73e946ce0bec026476560180
anmalkov <an.malkov@outlook.com>
2022-10-19 11:31:08 +0200

    allow remote access

--- src/Umbrella.Ui/ClientApp/src/setupProxy.js
+++ src/Umbrella.Ui/ClientApp/src/setupProxy.js
@@ -1,8 +1,8 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 const { env } = require('process');
 
-const target = env.ASPNETCORE_HTTPS_PORT ? `https://localhost:${env.ASPNETCORE_HTTPS_PORT}` :
-  env.ASPNETCORE_URLS ? env.ASPNETCORE_URLS.split(';')[0] : 'http://localhost:47293';
+const target = env.ASPNETCORE_HTTPS_PORT ? `https://0.0.0.0:${env.ASPNETCORE_HTTPS_PORT}` :
+  env.ASPNETCORE_URLS ? env.ASPNETCORE_URLS.split(';')[0] : 'http://0.0.0.0:47293';
 
 const context = [
     "/api/**",
